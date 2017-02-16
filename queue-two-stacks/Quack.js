87afc17b2681da5ae2c99a4b787d525a97cb1d3c
Justin Tan <jtanman9@gmail.com>
2017-02-15 23:59:43 -0800

    add queue-two-stacks

---
+++ queue-two-stacks/Quack.js
@@ -0,0 +1,21 @@
+// queue with 2 stacks
+// also known as a Quack
+
+function Quack() {
+  this.in = []
+  this.out = []
+}
+
+Quack.prototype.enqueue = function(item) {
+  this.in.push(item);
+}
+Quack.prototype.dequeue = function() {
+  if (this.out.length === 0) {
+    // move items in to out (reverse order)
+    while (this.in.length > 0) {
+      var newestInStackItem = this.in.pop();
+      this.out.push(newestInStackItem);
+    }
+  }
+  return this.out.pop();
+}
